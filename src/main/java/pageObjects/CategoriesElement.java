05717a5f6ad57d2956ea4a10b161e5c7bc8bfd83
GeraldAntonioJelves <gerald.jelves@scotiabank.com>
2023-09-11 21:13:39 -0300

    Se ejecuto un flujo automatizado para el registro del usuario

---
+++ src/main/java/pageObjects/CategoriesElement.java
@@ -0,0 +1,6 @@
+package main.java.pageObjects;
+
+public interface CategoriesElement {
+
+
+}
---
+++ src/main/java/pageObjects/NavComprasElements.java
@@ -0,0 +1,21 @@
+package main.java.pageObjects;
+
+public interface NavComprasElements {
+
+    //links
+    String linkBooks = "";
+    String linkComputers = "";
+    String linkElectronics = "";
+    String linkApparelAndShoes = "";
+    String linkDigitalDownloads = "";
+    String linkJewelry = "";
+    String linkGiftCards = "";
+    String computersDesktops = "";
+    String computersNotebooks = "";
+    String computersAccessories = "";
+
+    //texto
+    String textoDesktops = "";
+    String textoNotebooks = "";
+    String textoAccessories = "";
+}
